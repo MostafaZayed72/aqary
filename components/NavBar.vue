ba7448cd908e20d9d4b9844fde54e7515b311ab0
MostafaZayed72 <zayed8646@gmail.com>
2024-05-31 19:56:19 +0300

    feat: adjust navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,46 +1,67 @@
 <template>
-  <nav
-    class="shadow-lg h-20 flex justify-around items-center sticky top-0 z-10"
-    style="direction: rtl;"
-  >
-    <!-- START LOGO AND LINKS -->
-    <div
-      class="flex items-center gap-10 mx-6"
-
-    >
-      <NuxtLink
-        class="font-bold text-2xl md:text-3xl lg:text-5xl text-green-500 hover:text-green-400 hover:scale-105 delayed"
-        to="/"
-      >
-        عقاري</NuxtLink
-      >
-      <ul
-        class="font-bold text-xl lg:flex items-center gap-4 mt-2 cursor-pointer hidden"
-      >
-        <li
-          class="bg-gray-300 px-2 py-2 rounded hover:bg-gray-400 text-black delayed"
-        >
-          <NuxtLink to="NewShipment"> الرئيسية</NuxtLink>
-        </li>
-        <li
-          class="bg-gray-300 px-2 py-2 rounded hover:bg-gray-400 text-black delayed"
-        >
-          <NuxtLink to="/ShipmentsTrack">المشاريع الجديدة</NuxtLink>
-        </li>
-        <li
-          class="bg-gray-300 px-2 py-2 rounded hover:bg-gray-400 text-black delayed"
-        >
-          تواصل معنا
-        </li>
-      </ul>
+  <div class="md:flex justify-between items-center mx-4 hidden"
+    :class="$colorMode.preference == 'light' ? 'bg-cool-300' : ''" style="direction: rtl;">
+    <div class="flex gap-2 md:gap-8">
+      <div class="flex items-center cursor-pointer gap-1 hover:text-green-400 ">
+        <Icon name="material-symbols:settings-rounded" class="text-xl" />
+        <h1 class="font-bold">الإعدادات</h1>
+      </div>
+      <div class="flex items-center cursor-pointer gap-1 hover:text-green-400 ">
+        <Icon name="material-symbols:app-promo-sharp" class="text-xl" />
+        <h1 class="font-bold">تحميل التطبيق</h1>
+      </div>
     </div>
+
+
+    <div class="flex gap-2 md:gap-8">
+      <div class="flex items-center cursor-pointer gap-1 hover:text-green-400 ">
+        <Icon name="material-symbols:favorite" class="text-xl" />
+        <h1 class="font-bold">الإعلانات المفضلة</h1>
+      </div>
+      <div class="flex items-center cursor-pointer  hover:text-green-400 ">
+        <Icon name="material-symbols:person" class="text-xl" />
+        <Icon name="material-symbols:arrow-drop-down" class="text-xl" />
+       
+      </div>
+    </div>
+
+
+  </div>
+  <hr class="hidden md:block" />
+
+  <nav class="shadow-lg h-20 flex justify-around items-center " style="direction: rtl;">
+    <!-- START LOGO AND LINKS -->
+    
+    <!-- <div class="md:hidden cursor-pointer"><Icon name="iconamoon:menu-burger-horizontal-bold" class="text-2xl"/></div> -->
+ <MenuBurger />
+    <div class="flex items-center gap-10 mx-6">
+      <NuxtLink
+        class="font-bold text-3xl   text-green-500 hover:text-green-400 hover:scale-105 delayed flex items-center"
+        to="/">
+        عقاري
+        <Icon name="material-symbols:garage-home" />
+      </NuxtLink>
+      <ColorMode class="hidden md:block" />
+
+
+
+    </div>
+    <ColorMode class="md:hidden" />
     <!-- END LOGO AND LINKS-->
     <!-- START OTHER OPTIONS -->
-    <div
-      class="flex items-center gap-4"
-    >
-      <ColorMode />
+    <div class="md:flex items-center gap-4 hidden">
+      <ul class="font-bold text-xl flex items-center gap-4 mt-2 cursor-pointer ">
+        <li class="bg-gray-300 px-2 py-2 rounded hover:bg-green-400 text-black delayed">
+          <NuxtLink to="NewShipment"> المدونة</NuxtLink>
+        </li>
+        <li class="bg-gray-300 px-2 py-2 rounded hover:bg-green-400 text-black delayed">
+          <NuxtLink to="/ShipmentsTrack">الشركات العقارية</NuxtLink>
+        </li>
+        <li class="bg-gray-300 px-2 py-2 rounded hover:bg-green-400 text-black delayed">
+          المشاريع الجديدة </li>
+      </ul>
     </div>
+
     <!-- END OTHER OPTIONS -->
   </nav>
 </template>
---
+++ components/menuBurger.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="text-center" style="direction: rtl;">
+        <v-menu style="direction: rtl;">
+            <template v-slot:activator="{ props }">
+                <Icon name="iconamoon:menu-burger-horizontal-bold" class="text-2xl" v-bind="props" />
+            </template>
+
+            <v-list>
+                <v-list-item>
+                    <v-list-item-title>المدونة</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title>الشركات العقارية</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title>المشاريع الجديدة</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title>الإعلانات المفضلة</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title>الإعدادات</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title>تحميل التطبيق</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        items: [
+            { title: 'المدونة' },
+            { title: 'الشركات العقارية' },
+            { title: 'المشاريع الجديدة' },
+            { title: 'الإعلانات المفضلة' },
+            { title: 'الإعدادات' },
+            { title: 'تحميل التطبيق' }
+        ],
+    }),
+}
+</script>
