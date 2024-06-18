53c8f07972075a56d695821676b6753ce488fb73
MostafaZayed72 <zayed8646@gmail.com>
2024-06-19 00:55:26 +0300

    sign up

--- components/User/Sign.vue
+++ components/User/Sign.vue
@@ -1,85 +1,44 @@
 <template>
   <div class="pa-4 text-center w-100" :style="$i18n.locale == 'ar-AR' ? 'direction:rtl' : ''">
-    <v-dialog
-      v-model="dialog"
-      max-width="600"
-    >
+    <v-dialog v-model="dialog" max-width="600">
       <template v-slot:activator="{ props: activatorProps }">
-        <v-btn
-          class="text-none font-weight-regular"
-          prepend-icon="mdi-account"
-          :text="$t('Login')"
-          variant="tonal"
-          v-bind="activatorProps"
-          style="background-color: transparent"
-        ></v-btn>
+        <v-btn class="text-none font-weight-regular" prepend-icon="mdi-account" :text="$t('Login')" variant="tonal"
+          v-bind="activatorProps" style="background-color: transparent"></v-btn>
       </template>
 
       <!-- SIGN IN -->
-      <v-card
-        prepend-icon="mdi-account"
-        :title="$t('Login')"
-      >
-        <v-text-field
-          :label="$t('E-mail*')"
-          required
-        ></v-text-field>
-        <v-text-field
-          label="Password*"
-          type="password"
-          required
-        ></v-text-field>
-        <small 
-          class="text-caption text-medium-emphasis mx-4 underline cursor-pointer hover:text-teal-400"
-          @click="openForgotPasswordDialog"
-        >{{ $t('Forget Password ?') }}</small>
+      <v-card prepend-icon="mdi-account" :title="$t('Login')">
+        <v-text-field :label="$t('E-mail*')" required></v-text-field>
+        <v-text-field label="Password*" type="password" required></v-text-field>
+        <small class="text-caption text-medium-emphasis mx-4 underline cursor-pointer hover:text-teal-400"
+          @click="openForgotPasswordDialog">{{ $t('Forget Password ?') }}</small>
         <h1 class="text-center my-2 font-bold text-xl">{{ $t('Or Sign Up') }}</h1>
 
         <!-- SIGN UP -->
         <v-card-text>
           <v-row dense>
             <v-col cols="12" md="4" sm="6">
-              <v-text-field
-                v-model="userName"
-                :label="$t('Username*')"
-                required
-              ></v-text-field>
+              <v-text-field v-model="userName" :label="$t('Username*')" required></v-text-field>
             </v-col>
 
             <v-col cols="12" md="4" sm="6">
-              <v-text-field
-                v-model="mobile"
-                hint="example of helper text only on focus"
-                :label="$t('Mobile*')"
-              ></v-text-field>
+              <v-text-field v-model="mobile" hint="example of helper text only on focus"
+                :label="$t('Mobile*')"></v-text-field>
             </v-col>
 
             <v-col cols="12" md="4" sm="6"></v-col>
 
             <v-col cols="12" md="4" sm="6">
-              <v-text-field
-                v-model="email"
-                :label="$t('Email*')"
-                required
-              ></v-text-field>
+              <v-text-field v-model="email" :label="$t('Email*')" required></v-text-field>
             </v-col>
 
             <v-col cols="12" md="4" sm="6">
-              <v-text-field
-                v-model="password"
-                :label="$t('Password*')"
-                type="password"
-                required
-              ></v-text-field>
+              <v-text-field v-model="password" :label="$t('Password*')" type="password" required></v-text-field>
             </v-col>
 
             <v-col cols="12" md="4" sm="6">
-              <v-text-field
-                v-model="confirmPassword"
-                :label="$t('Confirm Password*')"
-                type="password"
-                required
-              ></v-text-field>
+              <v-text-field v-model="confirmPassword" :label="$t('Confirm Password*')" type="password"
+                required></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="6"></v-col>
@@ -95,74 +54,39 @@
         <v-card-actions>
           <v-spacer></v-spacer>
 
-          <v-btn
-            :text="$t('Close')"
-            variant="plain"
-            @click="dialog = false"
-          ></v-btn>
+          <v-btn :text="$t('Close')" variant="plain" @click="dialog = false"></v-btn>
 
-          <v-btn
-            color="primary"
-            :text="$t('Confirm')"
-            variant="tonal"
-            @click="SignUp"
-          ></v-btn>
+          <v-btn color="primary" :text="$t('Confirm')" variant="tonal" @click="SignUp"></v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
     <!-- Forget Password Dialog -->
-    <v-dialog
-      v-model="forgotPasswordDialog"
-      max-width="400"
-    >
-      <v-card
-        prepend-icon="mdi-lock-reset"
-        :title="$t('Reset Password')"
-      >
+    <v-dialog v-model="forgotPasswordDialog" max-width="400">
+      <v-card prepend-icon="mdi-lock-reset" :title="$t('Reset Password')">
         <v-card-text>
-          <v-text-field
-            :label="$t('Enter your email')"
-            required
-          ></v-text-field>
+          <v-text-field :label="$t('Enter your email')" required></v-text-field>
         </v-card-text>
 
         <v-card-actions>
           <v-spacer></v-spacer>
 
-          <v-btn
-            :text="$t('Cancel')"
-            variant="plain"
-            @click="forgotPasswordDialog = false"
-          ></v-btn>
+          <v-btn :text="$t('Cancel')" variant="plain" @click="forgotPasswordDialog = false"></v-btn>
 
-          <v-btn
-            color="primary"
-            :text="$t('Submit')"
-            variant="tonal"
-            @click="forgotPasswordDialog = false"
-          ></v-btn>
+          <v-btn color="primary" :text="$t('Submit')" variant="tonal" @click="forgotPasswordDialog = false"></v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
     <!-- Response Dialog -->
-    <v-dialog
-      v-model="responseDialog"
-      max-width="400"
-    >
+    <v-dialog v-model="responseDialog" max-width="400">
       <v-card>
-        <v-card-title class="headline">{{ $t('Response') }}</v-card-title>
         <v-card-text>
-          <p>{{ responseMessage }}</p>
+          <p>{{ $t(responseMessage) }}</p>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            :text="$t('Close')"
-            @click="responseDialog = false"
-          ></v-btn>
+          <v-btn color="primary" :text="$t('Close')" @click="responseDialog = false" class="text-center mx-auto"></v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -171,6 +95,7 @@
 
 <script setup>
 import { ref } from 'vue';
+const { t } = useI18n();
 
 // State variables
 const dialog = ref(false);
@@ -204,16 +129,17 @@ const SignUp = async () => {
       })
     });
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
+    if (response.ok) {
+      responseMessage.value = 'User successfully created and Verification Mail has been sent to the user';
+    } else {
+      const errorMessage = await response.json();
+      responseMessage.value = errorMessage.errors.Email[0] + ' / ' + errorMessage.errors.Email[1] + ' / ' + errorMessage.errors.Mobile[0] + ' / ' + errorMessage.errors.Mobile[1] +' / '+ errorMessage.errors.Password[0] +' / '+ errorMessage.errors.Password[1];
     }
 
-    const data = await response.json();
-    responseMessage.value = JSON.stringify(data);
     dialog.value = false;
     responseDialog.value = true;
   } catch (error) {
-    responseMessage.value = 'There was a problem with your fetch operation: ' + error.message;
+    responseMessage.value = error.message;
     responseDialog.value = true;
   }
 };
